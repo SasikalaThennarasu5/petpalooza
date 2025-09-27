df702b26f68ff02a5f93916338c3bb5bac99a295
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2025-09-27 15:18:54 +0530

    add

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 // Interceptor to attach access token only for protected routes
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
